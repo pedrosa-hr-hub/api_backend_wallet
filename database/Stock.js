bd8747117f5e2185671ba6754649d5c03e63467a
Pedro <pedro.trabalhos@outlook.com>
2022-06-23 19:27:07 -0300

    stock database model is ready

--- database/Stock.js
+++ database/Stock.js
@@ -2,16 +2,30 @@ const Sequelize = require('sequelize');
 const database = require('../database');
 
 const Stock = database.define('stock',{
+     id:{
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+     },
      ticker:{
-
+          type: Sequelize.STRING,
+          allowNull: false
      },
      price:{
-
+          type:Sequelize.DECIMAL(20,2),
+          allowNull: false
      },
      quanty:{
-     
+          type: Sequelize.INTEGER,
+          allowNull: false
      },
      datebuy:{
-          
+          type: Sequelize.DATE,
+          allowNull: false
      }
-})
+});
+
+Stock.sync({force: true});
+
+module.exports = Stock;
